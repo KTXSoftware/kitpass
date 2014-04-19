8bf314ee82e392c8b9a2386d1f7b395b61139a06
Robert Konrad <robert@ktx-software.com>
2014-04-19 02:59:02 +0200

    Read options.json.

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,20 +1,68 @@
-#include <fstream>
-#include <stdio.h>
+#include <Files.h>
+#include <Path.h>
+#include <Json.h>
+#include <FileReader.h>
+#include <StringHelper.h>
+#include <stdlib.h>
 #include <iostream>
 #include <string>
 
-bool startsWith(const std::string& s, const std::string& start) {
-	return s.substr(0, start.size()) == start;
-}
+using namespace kmd;
+
+enum ServerType {
+	GithubServer,
+	GitblitServer
+};
+
+struct Server {
+	ServerType type;
+	std::string urlpart;
+	std::string user;
+	std::string pass;
+};
 
 int main(int argc, char** argv) {
-	std::ofstream file("zeugs.txt");
-	file << "called" << "\n";
-	for (int i = 1; i < argc; ++i) {
-		file << argv[i] << "\n";
-	}
 	if (argc < 2) return 0;
+
+	std::vector<Server> servers;
+	std::string appdir = getenv("KIT_DATA_PATH");
+	Path options = Paths::get(appdir, "options.json");
+	if (Files::exists(options)) {
+		kmd::FileReader reader(options.toString().c_str());
+		char* content = (char*)reader.readAll();
+		char* text = new char[reader.size() + 1];
+		for (int i = 0; i < reader.size(); ++i) text[i] = content[i];
+		text[reader.size()] = 0;
+		Json::Data options(text);
+		int serverCount = options["servers"].size();
+		for (int i = 0; i < serverCount; ++i) {
+			Json::Value& server = options["servers"][i];
+			if (server.has("user") && server.has("pass")) {
+				Server s;
+				if (server["type"].string() == "gitblit") {
+					s.type = GitblitServer;
+					s.urlpart = server["url"].string();
+				}
+				else {
+					s.type = GithubServer;
+					s.urlpart = server["path"].string();
+				}
+				s.user = server["user"].string();
+				s.pass = server["pass"].string();
+				servers.push_back(s);
+			}
+		}
+	}
+
 	std::string request = argv[1];
-	if (startsWith(request, "Username")) printf("\n");
-	else if (startsWith(request, "Password")) printf("\n");
+	int urlstart = firstIndexOf(request, '\'');
+	int urlend = lastIndexOf(request, '\'');
+	std::string url = request.substr(urlstart + 1, urlend - urlstart - 1);
+	for (unsigned i = 0; i < servers.size(); ++i) {
+		if (contains(url, servers[i].urlpart)) {
+			if (startsWith(request, "Username")) std::cout << servers[i].user << std::endl;
+			else if (startsWith(request, "Password")) std::cout << servers[i].pass << std::endl;
+			break;
+		}
+	}
 }
